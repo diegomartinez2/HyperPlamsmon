f6e48aa808b26c8a0d8b5f8f35f893bd2cbc3750
diegomartinez2 <diegomartinez2@yahoo.es>
2024-03-04 08:01:28 +0100

    Alternative functions for T_c

--- Eliashberg.py
+++ Eliashberg.py
@@ -24,7 +24,8 @@
 # ---------------------------
 # Importación de los módulos
 # ---------------------------
-
+import numpy as np
+import matplotlib.pyplot as plt
 # -------
 # Clases
 # -------
@@ -235,7 +236,6 @@ class Eliashberg(object):
         """
         w_log calculated from Eliashberg
         """
-        #eV_to_K=11604
         #eV_to_K = 11604.5250061657
 
         #mask = self.w >=  0
@@ -277,8 +277,66 @@ class Eliashberg(object):
 # ----------
 # Funciones
 # ----------
-def NombredeFuncion(arg):
-    pass
+
+"""
+Alternate functions
+"""
+def T_c(w,mu_par,lambda_t,a2F):
+        """
+        Allen-Dynes formula (Advanced McMillan's equation and its application for the analysis of highly-compressed superconductors)
+        Tc=W_log/1.2*exp(-1.04*(1+self.lambda_2)/(self.lambda_2-mu_*(1+0.62*self.lambda_2)))
+        Note this is an approximation formula and is not accurate for some superconductors
+        formula from DOI 10.1007/s10948-017-4295-y
+        """
+        AllenDynes = True
+        out2=-1.04*(1+lambda_t)/(lambda_t-mu_par*(1+0.62*lambda_t))
+        if (AllenDynes):
+            print ("f1,f2=",f_1(mu_par,lambda_t),f_2(mu_par,w,lambda_t,a2F))
+            out=f_1(mu_par,lambda_t)*f_2(mu_par,w,lambda_t,a2F)*w_log(w,lambda_t,a2F)/1.2 * np.exp(out2)
+        else:
+            out=w_log(w,lambda_t,a2F)/1.2 * np.exp(out2)
+
+        return out/8.617333262e-5 #Boltzman constant in eV/K
+
+def w_log(w,lambda_t,a2F):
+        """
+        w_log calculated from Eliashberg
+        """
+        #eV_to_K=11604
+        #eV_to_K = 11604.5250061657
+
+        #mask = self.w >=  0
+        #mask = self.w >  0
+        #w = self.w#[mask]#*eV_to_K
+        #print("max(w)=",max(w))
+        for i in range(len(w)):
+          if w[i]<0:
+            print("error en w<0",i)
+            exit()
+        a2F_w = a2F/w #np.divide(a2F, w)
+        log_w = np.log(w)
+        w_log = np.exp((2.0/lambda_t)*np.trapz(a2F_w*log_w,w))
+            #integrate.simpson((np.divide(self.a2F_new(self.w), self.w)*np.log(self.w)),self.w))
+            #np.trapz(a2F_w*np.log(w)),w)
+        return w_log
+
+def w_2(w,lambda_t,a2F):
+        #eV_to_K=11604
+        #w = w#*eV_to_K
+        a2F_w = a2F*w
+        w_2 = np.sqrt((2.0/lambda_t)*
+            #integrate.simpson(
+            np.trapz(a2F_w,w))
+        return w_2
+
+def f_1(mu_par,lambda_t):
+        LAMBDA_temp = 2.46*(1+3.8*mu_par)
+        return np.power(1+np.power(lambda_t/LAMBDA_temp,3/2),1/3)
+
+def f_2(mu_par,w,lambda_t,a2F):
+        LAMBDA_temp =1.82*(1+6.3*mu_par)*(w_2(w,lambda_t,a2F)/w_log(w,lambda_t,a2F))
+        return 1 + (( w_2(w,lambda_t,a2F)/w_log(w,lambda_t,a2F) - 1) * lambda_t**2)/(
+        (lambda_t**2) + (LAMBDA_temp**2))
 
 
 
