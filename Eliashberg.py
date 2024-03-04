17d317d199327647197153b7d2316175a60f08aa
diegomartinez2 <diegomartinez2@yahoo.es>
2024-03-04 07:51:12 +0100

    New Eliashberg code

---
+++ Eliashberg.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  untitled.py
+#
+#  Copyright 2024 Diego Martinez Gutierrez <diego.martinez@ehu.eus>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+# ---------------------------
+# Importación de los módulos
+# ---------------------------
+
+# -------
+# Clases
+# -------
+class Eliashberg(object):
+    """docstring for NombredeClase."""
+
+    def __init__(self, arg):
+        super(NombredeClase, self).__init__()
+        self.arg = arg
+
+    def a2F_new(self,x,method = 0):
+        """
+        Calculates the Eliashberg spectral functions
+        ---input---
+        x: coordiate to calculate the Eliashberg function.
+        method: Method to use in the calculation.
+        ---output---
+        a2F = factor1*summa: The Eliashberg function at "x"
+        """
+
+        center = self.Omega[:] #*put units correctly...
+        width = self.Gamma[:] #*put units the same as center
+        gauss_width = 5*self.from_cm1_to_eV#(units.invcm/units.Hartree) #0.00002 # test the units of this... should be aprox. 5 cm-1 (1, 5 or 10)
+        if (method == 1):
+            #---------method1-------vvvv---
+            summa = 0
+            for i in range(len(self.qx)):
+                simmetry_factor =  2 #8
+                if self.qx[i] ==  0:
+                    simmetry_factor =  1 #4
+                    if self.qy[i] ==  0:
+                        simmetry_factor =  0 #1
+                elif self.qy[i] ==  0:
+                    simmetry_factor =  1 #4
+                elif self.qx[i] == self.qy[i]:
+                    simmetry_factor =  1 #4
+                summa += self.Lambda_q_new(i)*self.Omega[i] * self.gaussian(x,self.Omega[i],gauss_width) * simmetry_factor
+            #return summa/(2*(len(self.Omega)-self.indice_zeros))
+            return summa/(2*self.N)
+        else:
+            #---------method2------vvvv-
+            summa = 0
+
+            for i in range(len(self.qx)):
+                simmetry_factor =  2 #8
+                if self.qx[i] ==  0:
+                    simmetry_factor =  1 #4
+                    if self.qy[i] ==  0:
+                        simmetry_factor = 0 #1
+                elif self.qy[i] ==  0:
+                    simmetry_factor =  1 #4
+                elif self.qx[i] == self.qy[i]:
+                    simmetry_factor =  1 #4
+
+                summa += (self.Ratio[i]) * self.gaussian(x,self.Omega[i],gauss_width) * simmetry_factor
+            #return summa/(2*np.pi*self.N_ef*(len(self.Omega)-self.indice_zeros))
+            #return summa/(2*np.pi*self.N_ef*len(self.Omega))
+            #return summa/(2*np.pi*self.N_ef*self.N_qs)
+            return summa/(2*np.pi*self.N_ef*self.N)
+
+# ----------
+# Funciones
+# ----------
+def NombredeFuncion(arg):
+    pass
+
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
